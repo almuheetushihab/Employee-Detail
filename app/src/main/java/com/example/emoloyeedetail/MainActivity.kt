dc13570704a013d3fb8bb90378b7a09d6a86895e
Almuheetu Shihab <almuheetu@gmail.com>
2024-07-08 16:15:50 +0600

    changing font style

--- app/src/main/java/com/example/emoloyeedetail/MainActivity.kt
+++ app/src/main/java/com/example/emoloyeedetail/MainActivity.kt
@@ -49,5 +49,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+
+
     }
 }
