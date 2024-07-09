704e96c7a085e55b702dee1cae3941621396b505
Almuheetu Shihab <almuheetu@gmail.com>
2024-07-09 15:48:44 +0600

    changing App logo

--- app/src/main/java/com/example/emoloyeedetail/SecondFragment.kt
+++ app/src/main/java/com/example/emoloyeedetail/SecondFragment.kt
@@ -1,19 +1,15 @@
 package com.example.emoloyeedetail
-
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.emoloyeedetail.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
-    val args: SecondFragmentArgs by navArgs()
+    private val args: SecondFragmentArgs by navArgs()
     private lateinit var binding: FragmentSecondBinding
 
 
